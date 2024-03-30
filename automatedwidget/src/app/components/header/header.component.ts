09e64714545833ab4bf0a985218a3c10020970b4
anas <anasanasri3030@gmail.com>
2024-03-30 16:26:05 +0000

    fix logo

--- automatedwidget/src/app/components/header/header.component.ts
+++ automatedwidget/src/app/components/header/header.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  isMobile!: boolean; // Definite Assignment Assertion
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.checkIfMobile();
+    window.addEventListener('resize', () => {
+      this.checkIfMobile();
+    });
+  }
   reloadPage() {
     // Reload the page
     window.location.reload();
   }
+
+  toggleImage(hovering: boolean): void {
+    if (window.innerWidth > 1023){
+      if (hovering) {
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+      }
   
+  }
+
+  private checkIfMobile(): void {
+    this.isMobile = window.innerWidth <= 1023;
+  }
 }
