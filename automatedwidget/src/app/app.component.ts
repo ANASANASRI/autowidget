575088d315783fb5c3d87fa21ba9cd44f667bcb8
anas <anasanasri3030@gmail.com>
2024-03-31 01:01:36 +0000

    receive data

--- automatedwidget/src/app/app.component.ts
+++ automatedwidget/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amanpay-front-w';
-}
+
+  constructor() {
+      this.receiveMessageFromWidget();
+    }
+  
+    receiveMessageFromWidget() {
+      window.addEventListener('message', (event) => {
+        // Verify the sender origin
+        if (event.origin === 'https://anasanasri.github.io/WidgetTest/') {
+          // Access the data sent from the widget
+          const data = event.data;
+          console.log('Data received from widget:', data);
+          
+          // Process the received data here
+          // For example, you can set them to Angular component variables
+          // this.accessKey = data.accessKey;
+          // this.host = data.host;
+        }
+      });
+    }
+  }
