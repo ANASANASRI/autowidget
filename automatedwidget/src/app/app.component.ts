ff329c73676fa77f745838998b3f2484d7d0ecc1
anas <anasanasri3030@gmail.com>
2024-04-01 14:59:43 +0000

    received all data

--- automatedwidget/src/app/app.component.ts
+++ automatedwidget/src/app/app.component.ts
@@ -8,9 +8,13 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  
   accessKey: string | undefined;
   host: string | undefined;
+  merchantId: string | undefined;
+  orderId: string | undefined;
+  amount: number | undefined;
+  currency: string | undefined;
+  hmac: string | undefined;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -18,6 +22,15 @@ export class AppComponent implements OnInit {
     this.route.queryParams.subscribe(params => {
       this.accessKey = params['access_key'];
       this.host = params['host'];
+      this.merchantId = params['merchant_id'];
+      this.orderId = params['order_id'];
+      this.amount = params['amount'];
+      this.currency = params['currency'];
+      this.hmac = params['hmac'];
     });
+  
   }
+
+
+
   }
