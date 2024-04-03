f8863eeafe03db29ce31f2e9078aa7a3d2f24793
anas <anasanasri3030@gmail.com>
2024-04-03 14:46:19 +0000

    added payment method based on merchant ID

--- automatedwidget/src/app/components/header/header.component.ts
+++ automatedwidget/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { DataService } from '../../service/data.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { DataService } from '../../service/data.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'] // Changed 'styleUrl' to 'styleUrls'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit ,AfterViewInit{
   isMobile: boolean = false; // Initialize isMobile variable
   imageUrl: string = ''; // Declare imageUrl variable
 
@@ -19,7 +19,11 @@ export class HeaderComponent implements OnInit {
       this.checkIfMobile();
     });
   }
-
+  ngAfterViewInit(): void {
+    console.log('*************header**************:', this.dataService.merchantId);
+    // Do nothing here, let setUpSlider be called when the data is ready
+  }
+  
   reloadPage() {
     // Reload the page
     window.location.reload();
@@ -28,18 +32,18 @@ export class HeaderComponent implements OnInit {
   toggleImage(hovering: boolean): void {
     if (window.innerWidth > 1023) {
       if (hovering) {
-        this.imageUrl = 'https://i.ibb.co/yyyD2SV/Logohover.png'; // Change to your new image URL
+        this.imageUrl = 'https://i.ibb.co/yyyD2SV/Logohover.png'; 
       } else {
-        this.imageUrl = 'https://i.ibb.co/gFZGbV3/Logopng.png'; // Restore to the default image URL
+        this.imageUrl = 'https://i.ibb.co/gFZGbV3/Logopng.png'; 
       }
     } 
   }
   
   private setImageUrlForMobile(): void {
     if (this.isMobile) {
-      this.imageUrl = 'https://i.ibb.co/7K50gbw/Pay-Pikpng.png'; // Set the mobile image URL
+      this.imageUrl = 'https://i.ibb.co/7K50gbw/Pay-Pikpng.png'; 
     } else {
-      this.imageUrl = 'https://i.ibb.co/gFZGbV3/Logopng.png'; // Set the default image URL
+      this.imageUrl = 'https://i.ibb.co/gFZGbV3/Logopng.png';
     }
   }
   
