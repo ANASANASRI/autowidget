e769adb45bdf923eb12c9a362089d508310287cd
anas <anasanasri3030@gmail.com>
2024-04-15 14:28:56 +0100

    make the Steps Not Editable

--- automatedwidget/src/app/components/token/steps/steps.component.ts
+++ automatedwidget/src/app/components/token/steps/steps.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'; 
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-steps',
@@ -7,10 +11,11 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./steps.component.css'] 
 })
 export class StepsComponent implements OnInit {
+
   nameForm: FormGroup | any; 
   emailForm: FormGroup| any; 
 
-  constructor(private formBuilder: FormBuilder) { } 
+  constructor(private _formBuilder: FormBuilder) { } 
 
   ngOnInit() {
     this.initializeForms(); 
@@ -18,13 +23,24 @@ export class StepsComponent implements OnInit {
 
   initializeForms() {
     // Initialize name form with validation
-    this.nameForm = this.formBuilder.group({
+    this.nameForm = this._formBuilder.group({
       firstName: ['', Validators.required] // First name field is required
     });
 
     // Initialize email form with validation
-    this.emailForm = this.formBuilder.group({
+    this.emailForm = this._formBuilder.group({
       email: ['', [Validators.required, Validators.email]] 
     });
   }
+
+  ////////
+  isNameEditable: boolean = true;
+
+  // makeNameEditable() {
+  // this.isNameEditable = true;
+  // }
+  makeNameNoEditable() {
+    this.isNameEditable = false;
+  }
+
 }
