452cb67dc88cf6d5d0a9120a85522ba09ca541b7
anas <anasanasri3030@gmail.com>
2024-07-02 16:41:56 +0100

    Update the paypale component

--- automatedwidget/src/app/components/paypal/paypal.component.ts
+++ automatedwidget/src/app/components/paypal/paypal.component.ts
@@ -1,6 +1,8 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+declare var paypal: any; // Declare PayPal variable
+
 @Component({
   selector: 'app-paypal',
   templateUrl: './paypal.component.html',
@@ -19,8 +21,13 @@ export class PaypalComponent implements AfterViewInit {
   }
 
   loadPayPalScript(): Promise<void> {
-    return new Promise((resolve, reject) => {
+    return new Promise<void>((resolve, reject) => {
+      if (document.getElementById('paypal-sdk')) {
+        resolve(); // Script is already loaded
+        return;
+      }
       const script = document.createElement('script');
+      script.id = 'paypal-sdk';
       script.src = 'https://www.paypal.com/sdk/js?client-id=test&currency=USD';
       script.onload = () => resolve();
       script.onerror = () => reject(new Error('PayPal SDK script could not be loaded.'));
@@ -29,29 +36,30 @@ export class PaypalComponent implements AfterViewInit {
   }
 
   renderPayPalButton(): void {
-    // @ts-ignore
-    
     paypal.Buttons({
       style: {
         layout: 'horizontal',
-        color:  'blue',
-        label:  'pay',
+        color: 'blue',
+        label: 'pay',
         height: 40
       },
       createOrder: (data: any, actions: any) => {
-        return this.http.post<any>('/api/paypal/create-payment', {
+        return this.http.post<any>('/demo/checkout/api/paypal/order/create/', {
           amount: 1000.00, // Replace with the amount to be charged
-          currency: 'MAD', // Replace with the currency
+          currency: 'MAD', // Replace with the currency code
           description: 'Purchase description' // Replace with the description
         }).toPromise().then(order => {
           if (order && order.id) {
             return order.id;
           }
           throw new Error('Order creation failed');
+        }).catch(err => {
+          console.error('Order creation error', err);
+          throw err;
         });
       },
       onApprove: (data: any, actions: any) => {
-        return this.http.post<any>('/api/paypal/execute-payment', {
+        return this.http.post<any>('/demo/checkout/api/paypal/order/' + data.orderID + '/capture/', {
           paymentId: data.orderID,
           payerId: data.payerID
         }).toPromise().then(details => {
@@ -66,6 +74,5 @@ export class PaypalComponent implements AfterViewInit {
         console.error('PayPal Button Error', err);
       }
     }).render('#paypal-button-container');
-    
   }
 }
